d44da9a288d7e5576559e1166beea75ce2e48340
Szymon Czarnecki <spirit_sc@thinkdifferent.pl>
2014-10-26 12:23:36 +0100

    devise

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,1 +1,2 @@
 Errbit::Application.config.secret_token = 'f258ed6926x6dc8ad0ca79363c3d2f945c388a9c5920fc9a1ae99a98fbb619f135001c6434849b625884a9405a60cd3d50fc3e3b07ecd38cbed7406a4fccdb59c'
+Devise.secret_key = Errbit::Application.config.secret_token
